1eafb1743624aeb3e9e8536db12d0ba49ced7e13
JoAcO3412 <joaquindevige25@gmail.com>
2026-08-26 18:11:52 -0300

    Responsive terminado y GitHub Pages arreglado

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -57,22 +57,21 @@ window.addEventListener('scroll', () => {
 });
 
 
-// ==========================================
+
 // 2. LÓGICA DEL FORMULARIO DE CONTACTO
-// ==========================================
+
 document.getElementById('formContacto').addEventListener('submit', function(e) {
-    e.preventDefault(); // Evita que la página se recargue
+    e.preventDefault(); 
     
     const boton = this.querySelector('button');
     const textoOriginal = boton.innerText;
     
-    // Simulación de envío
     boton.innerText = 'Enviando...';
     boton.style.opacity = '0.7';
     
     setTimeout(() => {
         alert('¡Gracias por tu mensaje! Nos pondremos en contacto a la brevedad.');
-        this.reset(); // Limpia el formulario
+        this.reset(); 
         boton.innerText = textoOriginal;
         boton.style.opacity = '1';
     }, 1500);
@@ -81,7 +80,6 @@ document.getElementById('formContacto').addEventListener('submit', function(e) {
 
 const elementosAnimados = document.querySelectorAll('.fade-in');
 
-// Creamos el observador
 const observador = new IntersectionObserver((entradas) => {
     entradas.forEach((entrada) => {
         // Si el elemento entra en la pantalla
@@ -101,4 +99,26 @@ const observador = new IntersectionObserver((entradas) => {
 // Le indicamos al observador que vigile cada elemento
 elementosAnimados.forEach((elemento) => {
     observador.observe(elemento);
+});
+
+const menuToggle = document.getElementById('menuToggle');
+const headerInfo = document.querySelector('.header-info');
+
+menuToggle.addEventListener('click', () => {
+    // 1. Despliega el menú de enlaces
+    headerInfo.classList.toggle('activo');
+    
+    // 2. Activa la animación de la "X" en las 3 barritas
+    menuToggle.classList.toggle('abierto');
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    
+    // Si bajamos más de 50 píxeles, le agrega la clase 'scrolled'
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
 });
